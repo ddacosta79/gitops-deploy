6ec7262bb32321bdb4cdd8f597506b4e15ff4934
David Da Costa <ddacosta079@gmail.com>
2021-02-03 18:40:58 +0000

    managing env

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello wonderful world. Welcome to IBM, RedHat and Opensource!\n David working with Openshift');
+  res.send('Hello wonderful world. Welcome to IBM, RedHat and Opensource! David working with Openshift\n');
 });
 
 app.listen(8080, function () {
