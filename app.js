163bd9e045a6d531c64a4dbb621a6125b7aabab3
David Da Costa <ddacosta079@gmail.com>
2021-05-10 12:34:34 +0100

    new hello message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello wonderful world. Welcome to IBM, RedHat and Opensource! David working with Openshift\n');
+  res.send('Hello wonderful world. Welcome to IBM, RedHat and Opensource!\n');
 });
 
 app.listen(8080, function () {
